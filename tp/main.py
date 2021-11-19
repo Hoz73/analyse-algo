e9c47197c324b0c40d8d05f34b3f447708206a93
Hamze <hamzaalrashed1@hotmail.com>
2021-11-19 14:58:18 +0100

    implemented the second question

--- tp/main.py
+++ tp/main.py
@@ -1,15 +1,18 @@
 from question_one import problem_one
-from question_two import min_livraison_plusieurs_villes
 from question_two import problem_two
 
 if __name__ == '__main__':
-    # question 1 :
-    # problem_one('Cities10.txt')
-
-    # question 2 :
-    villes = [1,2,3,4,5,6,7]
-    start = 0
-    end = 9
-    # res = min_livraison_plusieurs_villes(start, end, villes)
-    # print(res)
-    problem_two(start, end, villes)
+    print(" 1 - first question \n"
+          " 2 - second question")
+    choix = input("chose your exercise : ")
+    if choix == '1':
+        # question 1 :
+        problem_one('Cities10.txt')
+    elif choix == '2':
+        # question 2 :
+        villes = [1, 2, 3, 4, 5]
+        start = 0
+        end = 9
+        problem_two(start, end, villes)
+    else:
+        print('invalid choice')
--- tp/question_two.py
+++ tp/question_two.py
@@ -1,8 +1,6 @@
-from question_one import get_distance
-from question_one import distances_between_cities
-from math import factorial
-from itertools import combinations
 from itertools import permutations
+from question_one import distances_between_cities
+from question_one import get_distance
 
 file_name = 'Cities10.txt'
 VILLES, res = distances_between_cities(file_name)
@@ -11,44 +9,18 @@ NB_VILLE = res['nb_villes']
 VILLES_CALCULEES = []
 
 
-def print_villes():
-    for i in VILLES:
-        print(i)
-
-
 def min_livraison_non_ville(start, end):
     distance = get_distance(VILLES, start, end)
     print(f'the distance between {start} and {end} is {distance}')
     return {'start': start, 'end': end, 'passing_by': [], 'distance': distance}
 
 
-def min_livraison_une_ville_OK(start, end, villes):
+def min_livraison_une_ville(start, end, villes):
     min_distance = get_distance(VILLES, start, villes[0]) + get_distance(VILLES, villes[0], end)
     print(
         f'you start at {start}, you pass by {villes[0]} with distance {get_distance(VILLES, start, villes[0])}'
         f' and you finish at {end} with a total distance {min_distance} ')
     return {'start': start, 'end': end, 'passing_by': [villes[0]], 'distance': min_distance}
-
-
-def min_livraison_une_ville(start, end, villes):
-    min_distance = get_distance(VILLES, start, villes[0]) + get_distance(VILLES, villes[0], end)
-    chosen_city = villes[0]
-    print(
-        f'distance from {start} passing by {villes[0]} tel {end} is :'
-        f' {get_distance(VILLES, start, villes[0]) + get_distance(VILLES, villes[0], end)}')
-    for i in range(1, len(villes)):
-        new_disance = get_distance(VILLES, start, villes[i]) + get_distance(VILLES, villes[i], end)
-        if new_disance < min_distance:
-            min_distance = new_disance
-            chosen_city = villes[i]
-        print(
-            f'distance from {start} passing by {villes[i]} tel {end} is :'
-            f' {get_distance(VILLES, start, villes[i]) + get_distance(VILLES, villes[i], end)}')
-    print(f'the minimal distance is to pass by {chosen_city}')
-    print(
-        f'you start at {start}, you pass by {chosen_city} with distance {get_distance(VILLES, start, chosen_city)}'
-        f' and you finish at {end} with a total distance {min_distance} ')
-    return {'start': start, 'end': end, 'villes': chosen_city}
 
 
 def check_if_calculated(start, end, villes):
@@ -66,13 +38,13 @@ def check_if_calculated(start, end, villes):
 
 def min_livraison_plusieurs_villes(start, end, villes):
     if len(villes) == 1:
-        print(start,end,villes)
+        print(start, end, villes)
         result = check_if_calculated(start, end, villes)
         if result is not None:
             print(f'already exist : {result}')
             return result['distance']
         else:
-            result = min_livraison_une_ville_OK(start, end, villes)
+            result = min_livraison_une_ville(start, end, villes)
             VILLES_CALCULEES.append(result)
             print(f'{result}')
             return result['distance']
@@ -84,28 +56,29 @@ def min_livraison_plusieurs_villes(start, end, villes):
             res = min_livraison_non_ville(villes[-1], end)
             VILLES_CALCULEES.append(res)
             distance = res['distance']
-        print(start, end , villes)
-        print(start, villes[-1] , villes[0:-1])
+        print(f'start : {start}, end : {end}, villes :{villes}')
+        print(f'start : {start}, end : {villes[-1]}, villes :{villes[0:-1]}')
         return distance + min_livraison_plusieurs_villes(start, villes[-1], villes[0:-1])
 
-def problem_two(start,end,villes):
+
+# Complexity factorial
+def problem_two(start, end, villes):
     result = list(permutations(villes))
     final_reults = []
     cities = []
     for villes in result:
-        v=[]
+        v = []
         for ville in villes:
             v.append(ville)
         cities.append(v)
-    len(cities)
     for tab in cities:
         res = min_livraison_plusieurs_villes(start, end, tab)
         final_reults.append({
             'start': start,
-            'end' : end,
+            'end': end,
             'ordre_passing_by': tab,
             'distance': res
         })
     print('##########################')
-    min_result = min(final_reults,key=lambda x:x['distance'])
+    min_result = min(final_reults, key=lambda x: x['distance'])
     print(f'and the minimal distance is {min_result["distance"]}, by passing by {min_result["ordre_passing_by"]}')
